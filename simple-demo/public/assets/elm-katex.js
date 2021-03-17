6c393fbf75cf144c37351733417f8e3dcb597b67
James Carlson <jxxcarlson@gmail.com>
2021-03-17 09:03:27 -0400

    - Update katex from 0.12.0 to 0.13.0 - Add to exposed modules for documentation

--- simple-demo/public/assets/elm-katex.js
+++ simple-demo/public/assets/elm-katex.js
@@ -4,12 +4,14 @@ const init =  async function(app) {
   var katexJs = document.createElement('script')
   katexJs.type = 'text/javascript'
   katexJs.onload = initKatex
-  katexJs.src = "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"
+  katexJs.src = "https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.js"
 
   document.head.appendChild(katexJs);
   console.log("elm-katex: I have appended katexJs to document.head");
 
 }
+
+
 
 function initKatex() {
 
@@ -31,7 +33,7 @@ function initKatex() {
         );
       let link = document.createElement('link');
       link.setAttribute('rel', 'stylesheet');
-      link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css');
+      link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css');
       this.shadowRoot.appendChild(link);
 
     }
